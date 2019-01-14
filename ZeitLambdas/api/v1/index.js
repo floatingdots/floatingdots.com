b90e6168759c07feed3490fb4d281c2cc4d667e4
Naoto Yoshimoto <naotone@floatingdots.com>
2019-01-14 03:08:11 -0500

    Add deploy api
    Get a webhook and trigger circleCi jobs.

--- ZeitLambdas/api/v1/index.js
+++ ZeitLambdas/api/v1/index.js
@@ -3,5 +3,6 @@ const express = require('express')
 const router = express.Router()
 
 router.use('/message', require('./message'))
+router.use('/deploy', require('./deploy'))
 
 module.exports = router
