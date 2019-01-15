28618ddc5fb04e3e78ccbc9ae76f0b6dbe6506db
Naoto Yoshimoto <naotone@floatingdots.com>
2019-01-15 17:23:40 -0500

    Add blog index and single page

--- Frontend/pages/blog/_slug.vue
+++ Frontend/pages/blog/_slug.vue
@@ -1,66 +1,129 @@
-<!-- prettier-ignore -->
 <template>
-  <article>
-    <header>
-      <h1>{{ post.title[$i18n.locale] }}</h1>
-      <ul class="categories">
-        <li
-          v-for="(category, i) in post.categories"
-          :key="i"
-        >
-          <NuxtLink :to="`tag/${category.slug.current}`">
-            <Tag :name="category.title[$i18n.locale]" />
-          </NuxtLink>
-        </li>
-      </ul>
-    </header>
-    <div v-html="bodyHtml" />
-  </article>
+  <div>
+    <LinkButton class="back" text="backToBlogList" url="blog" />
+    <article>
+      <header class="header">
+        <SanityImage
+          v-if="post.thumbnail"
+          :image="post.thumbnail"
+          :w="400"
+          :original-width="post.dimensions.width"
+          :original-height="post.dimensions.height"
+          :sizes="{
+            l: { media: 1024, width: 1024 },
+            m: { media: 768, width: 768 },
+            s: { media: 0, width: 400 },
+          }"
+          :alt="post.thumbnail.alt"
+          class="thumbnail"
+          role="presentation"
+          fit="clip"
+        />
+        <h1 class="title">{{ post.title[$i18n.locale] }}</h1>
+        <div class="info">
+          <div class="date">
+            <time :datetime="post.publishedAt">Posted: {{ date }}</time>
+          </div>
+          <ul class="category">
+            <li v-for="(category, i) in post.categories" :key="i">
+              <!-- <nuxt-link :to="`${category.slug.current}`"> -->
+              {{ category.title[$i18n.locale] }}
+              <!-- </nuxt-link> -->
+            </li>
+          </ul>
+        </div>
+      </header>
+      <hr />
+
+      <Body :content="post.body" />
+    </article>
+    <hr />
+
+    <Contact />
+  </div>
 </template>
 
 <script>
 import { sanity } from '@/plugins/sanity'
-import blocksToHtml from '@sanity/block-content-to-html'
+import LinkButton from '@/components/LinkButton'
+import SanityImage from '@/components/SanityImageLQIP'
+import Body from '@/components/Body'
+import Contact from '@/components/Contact'
 
 const queryPost = `
   *[_type == 'blog' && slug.current == $slug && !(_id in path("drafts.**"))]{
     ...,
-  }[0]
-`
+    categories[]->,
+    'dimensions': thumbnail.asset->metadata.dimensions,
+  }[0]`
 
 export default {
   async asyncData(context) {
     const post = await sanity.fetch(queryPost, context.route.params)
     return { post }
   },
-  components: {},
+  components: {
+    LinkButton,
+    SanityImage,
+    Body,
+    Contact,
+  },
   data() {
     return {
-      body: false,
+      date: '',
     }
   },
-  computed: {
-    bodyHtml() {
-      if (!this.post || !this.post.body) {
-        return '…'
-      }
-      return blocksToHtml({
-        blocks: this.post.body[this.$i18n.locale],
-        dataset: sanity.clientConfig.dataset,
-        projectId: sanity.clientConfig.projectId,
-        imageOptions: { w: this.width, h: this.height, fit: 'max' },
-      })
+  created() {
+    this.formattedDate(this.post.publishedAt)
+  },
+  methods: {
+    formattedDate(input) {
+      this.$moment.locale(this.$i18n.locale)
+      this.date = this.$moment(input).format('LL')
     },
   },
-  methods: {},
 }
 </script>
 
-<style lang="sass">
-article
-  ul
-    list-style: outside
-    margin: 0 0 4.8rem 2.4rem
-</style>
+<style lang="sass" scoped>
+.back
+  margin-bottom: 4rem
+.mainImage
+  margin: 0 0 4rem 0
+.title
+  +font-mobile-xxlarge
+  margin-top: 0
+  text-transform: uppercase
+  text-align: center
+  margin: 4rem auto
 
-<style lang="sass" scoped></style>
+.info
+  +font-mobile-small
+  text-transform: uppercase
+  text-align: center
+  color: #777
+  font-weight: 700
+
+.list
+  list-style: none
+  margin: 0
+  +font-mobile-base
+  font-weight: 500
+  .item
+    display: flex
+    flex-direction: column
+    margin: 0 0 2.4rem 0
+  .label
+    font-weight: 700
+
+@media only screen and (min-width: 48em)
+  .back
+    margin-bottom: 8rem
+  .info
+    +font-desktop-small
+    font-weight: 700
+  .title
+    +font-desktop-huge
+    max-width: 760px
+    margin: 8rem auto
+</style>
--- Frontend/pages/blog/index.vue
+++ Frontend/pages/blog/index.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="container">
+    <LinkButton class="back" text="backToHome" url="index" />
     <section class="blog">
       <span class="sectionTitle"> Blog </span>
       <ul class="posts">
-        <li v-for="(post, i) in blog" :key="i" class="post">
+        <li v-for="(post, i) in posts" :key="i" class="post">
           <NuxtLink
             :to="{
               name: `blog-slug___${$i18n.locale}`,
@@ -16,18 +17,31 @@
               :image="post.thumbnail"
               :w="400"
               :h="300"
+              :original-width="post.dimensions.width"
+              :original-height="post.dimensions.height"
               :sizes="{
-                l: { media: 64, width: 1200, height: 300 },
-                m: { media: 48, width: 800, height: 300 },
-                s: { media: 0, width: 400, height: 300 },
+                l: {
+                  media: 1024,
+                  width: 1024,
+                  height: 360,
+                },
+                m: {
+                  media: 768,
+                  width: 768,
+                  height: 300,
+                },
+                s: {
+                  media: 0,
+                  width: 400,
+                  height: 250,
+                },
               }"
               :alt="post.thumbnail.alt"
               role="presentation"
-              fit="clip"
+              fit="crop"
             />
             <span class="title"> {{ post.title[$i18n.locale] }} </span>
           </NuxtLink>
-          <hr />
         </li>
       </ul>
     </section>
@@ -36,138 +50,37 @@
 
 <script>
 import { sanity } from '@/plugins/sanity'
-import SanityImage from '@/components/SanityImage'
+import SanityImage from '@/components/SanityImageLQIP'
+import LinkButton from '@/components/LinkButton'
 
-const queryProjects = `
-  *[ _type == "projects" && !(_id in path("drafts.**")) ] | order(priority desc) | order(pudlishedAt desc){
-      availability,
-      title,
-      slug,
-      publishedAt,
-      thumbnail,
-    }[0..2]
-`
 const queryBlog = `
   *[ _type == "blog" && !(_id in path("drafts.**")) ] | order(pudlishedAt desc){
       title,
       slug,
       publishedAt,
       thumbnail,
-    }[0..2]
+      'dimensions': thumbnail.asset->metadata.dimensions,
+    }[0..10]
 `
 export default {
   async asyncData(context) {
-    const projects = await sanity.fetch(queryProjects, context.route.params)
-    const blog = await sanity.fetch(queryBlog, context.route.params)
-    return { projects, blog }
+    const posts = await sanity.fetch(queryBlog, context.route.params)
+    return { posts }
   },
   components: {
     SanityImage,
-  },
-  data() {
-    return {
-      items: ['Products', 'Team', 'Services', 'Company'],
-    }
+    LinkButton,
   },
 }
 </script>
 
 <style lang="sass" scoped>
+.back
+  margin-bottom: 4rem
 .sectionTitle
-  +font-mobile-huge
+  +font-mobile-xxlarge
   margin-bottom: 4.0rem
   display: block
-
-.intro
-  .heading
-    display: block
-    margin: 4.8rem 0 0 0
-    letter-spacing: 0.05rem
-    span
-      display: block
-      line-height: 1
-    .top
-      font-size: 2.8rem
-      font-weight: 700
-    .bottom
-      margin-top: 0.2rem
-      font-size: 4.2rem
-      font-weight: 700
-      display: flex
-      flex-direction: row
-    .your
-      margin-right: 0.8rem
-    .message
-      position: relative
-      li:nth-child(2)
-        animation-delay: 1200ms
-      li:nth-child(3)
-        animation-delay: 1200*2ms
-      li:nth-child(4)
-        animation-delay: 1200*3ms
-    .item
-      opacity: 0
-      position: absolute
-      animation: animate 1200*4ms ease-in-out infinite
-    @keyframes animate
-      0%
-        opacity: 0
-      5%
-        opacity: 0
-        transform: translateY(-5px)
-      10%
-        opacity: 1
-        transform: translateY(0px)
-      25%
-        opacity: 1
-        transform: translateY(0px)
-      30%
-        opacity: 0
-        transform: translateY(10px)
-      80%
-        opacity: 0
-      100%
-        opacity: 0
-
-  .title
-    +font-mobile-base
-    margin-bottom: 5.6rem
-
-.projects
-  .posts
-    .post
-      margin-bottom: 8.0rem
-      display: flex
-      flex-direction: column
-      .title
-        +font-mobile-xlarge
-        text-decoration: none
-        margin: 0 0 2.4rem 0
-      .content
-        display: flex
-        align-items: center
-      .soon
-        position: absolute
-        +font-mobile-large
-        max-width: 72%
-        z-index: 10
-      .thumbnail
-        margin: 0 0 0 auto
-        max-width: 72%
-        filter: drop-shadow(0px 20px 30px rgba(0, 0, 0, 0.2))
-        &.unavailable
-          opacity: 0.2
-          filter: none
-      &.unavailable
-        .thumbnail,
-        .title
-          pointer-events: none
-
-.weLove
-  +font-mobile-huge
-  font-size: 3.2rem
-  letter-spacing: 0.05rem
-
 .blog
   hr
     height: 0.4rem
@@ -187,29 +100,8 @@ export default {
     margin: 1.6rem 0
 
 @media only screen and (min-width: 48em)
-  .intro
-    .heading
-      margin-bottom: 2.4rem
-      .top
-        font-size: 4rem
-      .bottom
-        font-size: 8rem
-        font-weight: 700
-        margin-top: -1rem
-    .title
-      +font-desktop-medium
-      line-height: 1.2
-
-  .projects
-    .posts
-      .post
-        .title
-          +font-desktop-huge
+  .back
+    margin-bottom: 8rem
+  .sectionTitle
+    +font-desktop-xxlarge
 </style>
-
-<style lang="sass">
-.weLove
-  .small
-    +font-mobile-xlarge
-</style>
-s
